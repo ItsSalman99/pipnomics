16c8817dedecbfd8723d4cd1d45bdf68f62897f1
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2026-09-19 10:00:36 +0500

    fixes

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="/storage/logo.png" alt="Pipnomics" v-bind="$attrs" class="object-contain" />
+    <img src="/assets/logo.png" alt="Pipnomics" v-bind="$attrs" class="object-contain" />
 </template>
